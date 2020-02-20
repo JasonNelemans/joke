99a40ac2ee7fcf3e33809fc6fb7bde1876a8d2e4
JasonNelemans <jasonnelemans@hotmail.com>
2020-02-20 17:34:28 +0100

    changed port and style

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const app = express();
 const page = `<html>
 <head>
   <title>Jason Nelemans</title>
+  <style>
+    html {
+      background: linear-gradient(90deg, rgba(40,31,200,1) 0%, rgba(5,104,255,1) 25%, rgba(0,212,255,1) 100%);
+    }
+
+    .container {
+      color: #f98402;
+      text-align: center;
+      margin-top: 200px;
+    }
+
+    p {
+      font-size: 30px;
+    }
+  </style>
 </head>
 
 <body>
-  <h1>Why did the bike fall over?</h1>
-  <p>It was two tired</p>
+  <div class="container">
+    <h1>Why did the bike fall over?</h1>
+    <p>It was two tired</p>
+  </div>
 </body>
 </html>`
 
@@ -17,7 +34,7 @@ app.get('/', (request, response) => {
   response.send(page);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 function onListen() {
   console.log(`Listening on :${port}`);
